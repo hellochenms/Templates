5a99905354b05794b9af709ec858dd9293e93300
hellochenms <hellochenms@163.com>
2019-12-04 21:38:44 +0800

    m2-oc-common:vc

---
+++ File Templates/M2/m2-oc-common.xctemplate/UIViewControllerObjective-C/___FILEBASENAME___.m
@@ -0,0 +1,37 @@
+//___FILEHEADER___
+
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+#pragma mark - Life Cycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self addSubViews];
+    [self buildLayout];
+}
+
+- (void)addSubViews {
+    
+}
+
+- (void)buildLayout {
+    
+}
+
+
+#pragma mark - Event
+
+
+#pragma mark - Tools
+
+
+#pragma mark - Getter
+
+
+@end
